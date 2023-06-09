ef6a07db5fbe7503c8c5c8c7b99f65d18baed165
Ryu Yokoyama <yokoyamaryu1218@yahoo.co.jp>
2023-06-09 11:17:03 +0900

    初回のcommit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert(
+            [
+                [
+                    'name' => 'admin',
+                    'email' => 'admin@admin.com',
+                    'password' => Hash::make('password123'),
+                    'role' => 1,
+                ],
+                [
+                    'name' => 'test',
+                    'email' => 'test@test.com',
+                    'password' => Hash::make('password123'),
+                    'role' => 9,
+                ]
+            ],
+        );
+    }
+}
---
+++ resources/views/navigation-menu.blade.php
@@ -0,0 +1,7 @@
+@auth
+    <x-navigation-auth-menu />
+@endauth
+
+@guest
+    <x-navigation-guest-menu />
+@endguest
